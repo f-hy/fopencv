7c74b08b4ed3c1428f8a4c84284c09243031e3a9
f-hy <friendy2021@outlook.com>
2023-11-21 21:50:21 +0800

    pre-build version start

--- test/dg/9_1.cpp
+++ test/dg/9_1.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 #include<opencv2/opencv.hpp>
-#include<opencv2/cudaimgproc.hpp>
 #include "dg.h"
 using namespace std;
 using namespace cv;
-// #define dg "E:/data/courses/dg/"
-// #define dgdata dg"static/"
-// #define imgpath dgdata"9.11.png"
+#define dg "E:/data/courses/dg/"
+#define dgdata dg"static/"
+#define imgpath dgdata"9.11.png"
+//用cv::dft对输入图像（可以只考虑单通道图像）进行傅里叶变换，显示其功率谱，并测试非移中和移中的情况。
 
 int main() {
-  const String& s ="E:/apple.jpg";
-  Mat img = imread(s);
-  if(img.empty()) {
-    cout<<"IMG is empty"<<endl;
-    img = Mat(300, 300, CV_8UC3, Scalar(255, 0, 255));
-  }
-  // cout<<imgpath<<endl;
-  cout<<img.size<<endl;
+  Mat img  = imread(imgpath);
   // Mat img1 = testimage(img);
-  // Mat dftimg = dftImage(img);
+  Mat dftimg = dftImage(img);
   imshow("monalisa", img);
-  imwrite("E:/a.png", img);
-  // imshow("dftimg", dftimg);
+  imshow("dftimg", dftimg);
   // imshow("img1", img1);
   waitKey(0);
   destroyAllWindows();
-  cout << "hello";
-
   return 0;
 }
